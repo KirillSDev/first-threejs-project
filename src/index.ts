bf9d0f75bb666e4d12d0a34290da9c3653ced246
Kirill Starodubsev <colereteps@gmail.com>
2023-12-30 23:42:11 +0400

    feat: Added first scene

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,44 @@
-const body = document.querySelector('body');
+import * as THREE from 'three';
 
 
-body.innerHTML = "fef";
+// Canvas 
+
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+// Geometry
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+// Material
+const material = new THREE.MeshBasicMaterial({
+    color: '#FF0000',
+})
+
+// Mesh --> 
+const mesh = new THREE.Mesh(boxGeometry, material);
+
+scene.add(mesh);
+
+const sizes = {
+    width: 720,
+    height: 540,
+}
+
+//Camera 
+const camera = new THREE.PerspectiveCamera(65, sizes.width / sizes.height);
+camera.position.x = 0.5;
+camera.position.y = 0.5;
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer 
+
+const renderer = new THREE.WebGLRenderer({
+    canvas
+})
+
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
